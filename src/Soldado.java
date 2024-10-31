1a92744c755df1b37d8e453e14f862abf31946d3
juanjo380 <juan.bolanos.serna@correounivalle.edu.co>
2024-10-30 22:22:36 -0500

    inicializando clases

---
+++ src/Soldado.java
@@ -0,0 +1,15 @@
+public class Soldado {
+    private String nombre;
+    private String id;
+    private String rango;
+    
+    public void mostrarInformacion(){
+        
+    }
+
+    public Soldado(String nombre, String id, String rango) {
+        this.nombre = nombre;
+        this.id = id;
+        this.rango = rango;
+    }
+}
