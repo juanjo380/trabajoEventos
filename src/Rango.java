b2b9e1e8e8c0b1a4d128d86532005b828dc497ab
juanjo380 <juan.bolanos.serna@correounivalle.edu.co>
2024-11-04 22:17:24 -0500

    Organizando clases

--- src/Rango.java
+++ src/Rango.java
@@ -1,3 +1,10 @@
 public abstract class Rango {
+    protected int nivel;
     
+    //Constructor
+    public Rango(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public abstract void realizarAccion();
 }
