10a35310bf5693607c5350ae00bb37b41401818e
juanjo380 <juan.bolanos.serna@correounivalle.edu.co>
2024-11-26 23:44:22 -0500

    Continuacion miniproyecto2

---
+++ src/BaseDelValle.java
@@ -0,0 +1,19 @@
+import CategoriasMilitares.Capitan;
+import CategoriasMilitares.Coronel;
+import CategoriasMilitares.Soldado;
+import CategoriasMilitares.Teniente;
+import MisionesMilitares.OperacionesMilitares;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+
+
+    public class BaseDelValle {
+        public static void main(String[] args) {
+            AplicacionMilitar aplicacion = new AplicacionMilitar();
+            aplicacion.setVisible(true);
+        }
+    }
+
